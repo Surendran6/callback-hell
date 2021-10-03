1db70f3413197f6d923928b213851b68825a8963
Surendran6 <88974825+Surendran6@users.noreply.github.com>
2021-10-03 11:14:35 +0530

    Add files via upload

---
+++ callback hell/script.js
@@ -0,0 +1,34 @@
+let countdown = 10;
+setTimeout(()=>{
+    document.getElementById("display").innerHTML=countdown--;
+    setTimeout(()=>{
+        document.getElementById("display").innerHTML=countdown--;
+        setTimeout(()=>{
+            document.getElementById("display").innerHTML=countdown--;
+            setTimeout(()=>{
+                document.getElementById("display").innerHTML=countdown--;
+                setTimeout(()=>{
+                    document.getElementById("display").innerHTML=countdown--;
+                    setTimeout(()=>{
+                        document.getElementById("display").innerHTML=countdown--;
+                        setTimeout(()=>{
+                            document.getElementById("display").innerHTML=countdown--;
+                            setTimeout(()=>{
+                                document.getElementById("display").innerHTML=countdown--;
+                                setTimeout(()=>{
+                                    document.getElementById("display").innerHTML=countdown--;
+                                    setTimeout(()=>{
+                                        document.getElementById("display").innerHTML=countdown--;
+                                        setTimeout(()=>{
+                                            document.getElementById("display").innerHTML="Happy Independence Day ";
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)    
+},1000)
